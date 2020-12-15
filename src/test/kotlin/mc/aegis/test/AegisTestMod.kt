63d705b35e97f481df7cb1c03d142f4ae66ebabd
P03W <pweidman.mail@gmail.com>
2020-12-14 22:15:18 -0500

    Update license for real

--- src/test/kotlin/mc/aegis/test/AegisTestMod.kt
+++ src/test/kotlin/mc/aegis/test/AegisTestMod.kt
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package mc.aegis.test
 
 import com.mojang.brigadier.arguments.*
@@ -10,7 +16,7 @@ import net.minecraft.command.argument.Vec3ArgumentType
 class AegisTestMod : ModInitializer {
     override fun onInitialize() {
         println("Aegis test")
-        CommandRegistrationCallback.EVENT.register { dispatcher, dedicated ->
+        CommandRegistrationCallback.EVENT.register { dispatcher, _ ->
             dispatcher.register(AegisCommandBuilder("aegis") {
                 literal("literalTest") {
                     executes {
